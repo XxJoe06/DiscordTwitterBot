47b8bf3c4dfd7e9e0b43087286c7e578d658abe4
Snow <joejojogoat@gmail.com>
2023-08-08 01:11:11 +0530

    yes

--- controller/discord.js
+++ controller/discord.js
@@ -1,355 +1,335 @@
 const {
-	ActivityType,
-	ChannelType,
-	Client,
-	DiscordAPIError,
-	GatewayIntentBits,
-	Partials,
-	PermissionFlagsBits
+    ChannelType,
+    Client,
+    DiscordAPIError,
+    GatewayIntentBits,
+    Partials,
+    PermissionFlagsBits,
+    MessageActionRow,
+    MessageButton
 } = require("discord.js");
 
 const ignoredChannelTypes = [
-	ChannelType.GuildAnnouncement,
-	ChannelType.GuildCategory,
-	ChannelType.GuildNews,
-	ChannelType.GuildNewsThread,
-	ChannelType.GuildPrivateThread,
-	ChannelType.GuildPublicThread,
-	ChannelType.PrivateThread,
-	ChannelType.PublicThread
+    ChannelType.GuildAnnouncement,
+    ChannelType.GuildCategory,
+    ChannelType.GuildNews,
+    ChannelType.GuildNewsThread,
+    ChannelType.GuildPrivateThread,
+    ChannelType.GuildPublicThread,
+    ChannelType.PrivateThread,
+    ChannelType.PublicThread
 ];
 
 module.exports = class DiscordController extends require("./template.js") {
-	constructor () {
-		super();
+    constructor() {
+        super();
 
-		if (!app.Config.has("BOT_ID")) {
-			throw new Error("BOT_ID not set");
-		}
-		else if (!app.Config.has("DISCORD_TOKEN")) {
-			throw new Error("DISCORD_TOKEN not set");
-		}
+        if (!app.Config.has("BOT_ID")) {
+            throw new Error("BOT_ID not set");
+        } else if (!app.Config.has("DISCORD_TOKEN")) {
+            throw new Error("DISCORD_TOKEN not set");
+        }
 
-		this.#init();
+        this.#init();
 
-		this.data.permissions = PermissionFlagsBits;
-	}
+        this.data.permissions = PermissionFlagsBits;
+    }
 
-	#init () {
-		this.client = new Client({
-			intents: [
-				GatewayIntentBits.Guilds,
-				GatewayIntentBits.GuildMembers,
-				GatewayIntentBits.GuildMessages,
-				GatewayIntentBits.MessageContent
-			],
-			partials: [
-				Partials.Channel
-			]
-		});
+    #init() {
+        this.client = new Client({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMembers,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent
+            ],
+            partials: [
+                Partials.Channel
+            ]
+        });
 
-		this.initListeners();
+        this.initListeners();
 
-		const token = app.Config.get("DISCORD_TOKEN");
-		this.client.login(token);
-	}
+        const token = app.Config.get("DISCORD_TOKEN");
+        this.client.login(token);
+    }
 
-	initListeners () {
-		const client = this.client;
+    initListeners() {
+        const client = this.client;
 
-		client.on("ready", async () => {
-			app.Logger.info(`Logged in as ${client.user.tag}!`);
-			this.initGuilds();
+        client.on("ready", async () => {
+            app.Logger.info(`Logged in as ${client.user.tag}!`);
+            this.initGuilds();
+        });
 
-			const channelList = await app.Cache.get("twitter-channels");
+        client.on("messageCreate", async (messageObject) => {
+            const {
+                author,
+                channel,
+                channelType,
+                commandArgs,
+                guild,
+                message,
+                privateMessage
+            } = this.parseMessage(messageObject);
 
-			const options = [
-				{
-					name: `TwitterBot | ${app.Command.prefix}help`,
-					type: ActivityType.Playing
-				},
-				{
-					name: `${channelList.length} channels`,
-					type: ActivityType.Watching
-				},
-				{
-					name: `TwitterBot | ${this.client.guilds.cache.size} servers`,
-					type: ActivityType.Playing
-				}
-			];
+            if (privateMessage) {
+                return;
+            }
 
-			this.client.user.setPresence({
-				status: "online",
-				activities: [
-					{
-						...options[0]
-					}
-				]
-			});
-			
-			setInterval(() => {
-				const randomOption = options[Math.floor(Math.random() * options.length)];
-				this.client.user.setPresence({
-					status: "online",
-					activities: [
-						{
-							...randomOption
-						}
-					]
-				});
-			// every 5 minutes
-			}, 300000);
-		});
+            if (ignoredChannelTypes.includes(channelType)) {
+                return;
+            }
 
-		client.on("messageCreate", async (messageObject) => {
-			const {
-				author,
-				channel,
-				channelType,
-				commandArgs,
-				guild,
-				message,
-				privateMessage
-			} = this.parseMessage(messageObject);
+            const botPermissions = messageObject.channel.permissionsFor?.(app.Config.get("BOT_ID"));
+            if (botPermissions && !botPermissions.has(PermissionFlagsBits.SendMessages)) {
+                return;
+            }
 
-			if (privateMessage) {
-				return;
-			}
+            const serverData = await app.Cache.get(`discord-guilds-${guild.id}`);
+            if (app.Command.is(message, serverData)) {
+                const prefix = serverData.prefix ?? app.Command.prefix;
+                const command = message.replace(prefix, "").split(" ").find(Boolean);
+                const args = (commandArgs[0] === prefix)
+                    ? commandArgs.slice(2)
+                    : commandArgs.slice(1);
 
-			if (ignoredChannelTypes.includes(channelType)) {
-				return;
-			}
+                const userData = author;
+                const channelData = this.client.channels.cache.get(channel);
+                await this.handleCommand(
+                    command,
+                    args,
+                    channelData,
+                    userData,
+                    {
+                        guild,
+                        member: messageObject.member
+                    }
+                );
+            }
+        });
 
-			const botPermissions = messageObject.channel.permissionsFor?.(app.Config.get("BOT_ID"));
-			if (botPermissions && !botPermissions.has(PermissionFlagsBits.SendMessages)) {
-				return;
-			}
+        client.on("guildCreate", async (guild) => this.initGuild(guild));
 
-			const serverData = await app.Cache.get(`discord-guilds-${guild.id}`);
-			if (app.Command.is(message, serverData)) {
-				const prefix = serverData.prefix ?? app.Command.prefix;
-				const command = message.replace(prefix, "").split(" ").find(Boolean);
-				const args = (commandArgs[0] === prefix)
-					? commandArgs.slice(2)
-					: commandArgs.slice(1);
+        client.on("guildDelete", async (guild) => this.removeGuild(guild));
 
-				const userData = author;
-				const channelData = this.client.channels.cache.get(channel);
-				await this.handleCommand(
-					command,
-					args,
-					channelData,
-					userData,
-					{
-						guild,
-						member: messageObject.member
-					}
-				);
-			}
-		});
+        client.on("error", async (error) => {
+            if (error.toString().includes("TextChannel.send")) {
+                return;
+            }
 
-		client.on("guildCreate", async (guild) => this.initGuild(guild));
+            app.Logger.error(error);
+            this.restart();
+        });
 
-		client.on("guildDelete", async (guild) => this.removeGuild(guild));
+        // Handle button interactions
+        client.on('interactionCreate', async (interaction) => {
+            if (!interaction.isButton()) return;
 
-		client.on("error", async (error) => {
-			if (error.toString().includes("TextChannel.send")) {
-				return;
-			}
+            if (interaction.customId === 'tweet_button') {
+                await interaction.reply({
+                    content: `Here's the link to the tweet: ${interaction.message.embeds[0].url}`,
+                    ephemeral: true
+                });
+            }
+        });
+    }
 
-			app.Logger.error(error);
-			this.restart();
-		});
-	}
+    destroy() {
+        this.client && this.client.destroy();
+        this.client = null;
+    }
 
-	destroy () {
-		this.client && this.client.destroy();
-		this.client = null;
-	}
+    async initGuilds() {
+        const guilds = await this.client.guilds.fetch();
+        const cachedGuilds = await app.Cache.getKeysByPrefix("discord-guilds-*");
+        if (cachedGuilds.length === 0) {
+            for (const guild of guilds.values()) {
+                const { id, name } = guild;
+                const guildObject = {
+                    id,
+                    name,
+                    prefix: app.Command.prefix,
+                    channels: []
+                };
 
-	async initGuilds () {
-		const guilds = await this.client.guilds.fetch();
-		const cachedGuilds = await app.Cache.getKeysByPrefix("discord-guilds-*");
-		if (cachedGuilds.length === 0) {
-			for (const guild of guilds.values()) {
-				const { id, name } = guild;
-				const guildObject = {
-					id,
-					name,
-					prefix: app.Command.prefix,
-					channels: []
-				};
+                await app.Cache.setByPrefix(
+                    `discord-guilds-${id}`,
+                    guildObject,
+                    { expiry: 0 }
+                );
+            }
 
-				await app.Cache.setByPrefix(
-					`discord-guilds-${id}`,
-					guildObject,
-					{ expiry: 0 }
-				);
-			}
+            return;
+        }
 
-			return;
-		}
+        for (const cachedGuild of cachedGuilds) {
+            const guildId = cachedGuild.replace("discord-guilds-", "");
+            const guild = guilds.get(guildId);
+            if (!guild) {
+                await app.Cache.delete(cachedGuild);
+                app.Logger.info(`Left guild (${guildId}) ${cachedGuild.name}`);
 
-		for (const cachedGuild of cachedGuilds) {
-			const guildId = cachedGuild.replace("discord-guilds-", "");
-			const guild = guilds.get(guildId);
-			if (!guild) {
-				await app.Cache.delete(cachedGuild);
-				app.Logger.info(`Left guild (${guildId}) ${cachedGuild.name}`);
+                continue;
+            }
+        }
 
-				continue;
-			}
-		}
+        for (const guild of guilds.values()) {
+            const { id, name } = guild;
+            const cachedGuild = await app.Cache.getByPrefix(`discord-guilds-${id}`);
+            if (!cachedGuild) {
+                const guildObject = {
+                    id,
+                    name,
+                    prefix: app.Command.prefix,
+                    channels: []
+                };
 
-		for (const guild of guilds.values()) {
-			const { id, name } = guild;
-			const cachedGuild = await app.Cache.getByPrefix(`discord-guilds-${id}`);
-			if (!cachedGuild) {
-				const guildObject = {
-					id,
-					name,
-					prefix: app.Command.prefix,
-					channels: []
-				};
+                await app.Cache.setByPrefix(
+                    `discord-guilds-${id}`,
+                    guildObject,
+                    { expiry: 0 }
+                );
+            }
+        }
+    }
 
-				await app.Cache.setByPrefix(
-					`discord-guilds-${id}`,
-					guildObject,
-					{ expiry: 0 }
-				);
-			}
-		}
-	}
+    async initGuild(guild) {
+        const { id, name } = guild;
+        const cachedGuild = await app.Cache.getByPrefix(`discord-guilds-${id}`);
+        if (!cachedGuild) {
+            const guildObject = {
+                id,
+                name,
+                prefix: app.Command.prefix,
+                channels: []
+            };
 
-	async initGuild (guild) {
-		const { id, name } = guild;
-		const cachedGuild = await app.Cache.getByPrefix(`discord-guilds-${id}`);
-		if (!cachedGuild) {
-			const guildObject = {
-				id,
-				name,
-				prefix: app.Command.prefix,
-				channels: []
-			};
+            app.Logger.info(`Joined guild (${id}) ${name}`);
 
-			app.Logger.info(`Joined guild (${id}) ${name}`);
+            await app.Cache.setByPrefix(
+                `discord-guilds-${id}`,
+                guildObject,
+                { expiry: 0 }
+            );
+        }
+    }
 
-			await app.Cache.setByPrefix(
-				`discord-guilds-${id}`,
-				guildObject,
-				{ expiry: 0 }
-			);
-		}
-	}
+    async removeGuild(guild) {
+        const { id } = guild;
+        await app.Cache.delete(`discord-guilds-${id}`);
 
-	async removeGuild (guild) {
-		const { id } = guild;
-		await app.Cache.delete(`discord-guilds-${id}`);
+        app.Logger.info(`Left guild (${id}) ${guild.name}`);
+    }
 
-		app.Logger.info(`Left guild (${id}) ${guild.name}`);
-	}
-	
-	async send (message, channel, options = {}) {
-		const channelObject = await this.client.channels.fetch(channel.id);
-		if (!channelObject) {
-			return;
-		}
+    async send(message, channel, options = {}) {
+        const channelObject = await this.client.channels.fetch(channel.id);
+        if (!channelObject) {
+            return;
+        }
 
-		const sendTarget = {
-			embeds: []
-		};
+        const sendTarget = {
+            embeds: [],
+            components: options.components ?? [] // Adding components to the send target
+        };
 
-		if (Array.isArray(options.embeds) && options.embeds.length !== 0) {
-			sendTarget.embeds = options.embeds;
-		}
-		if (typeof message === "string") {
-			message = message.replace(/\\g/, "\\\\");
-			sendTarget.content = message;
-		}
+        if (Array.isArray(options.embeds) && options.embeds.length !== 0) {
+            sendTarget.embeds = options.embeds;
+        }
+        if (typeof message === "string") {
+            message = message.replace(/\\g/, "\\\\");
+            sendTarget.content = message;
+        }
 
-		try {
-			await channelObject.send(sendTarget);
-		}
-		catch (e) {
-			if (e instanceof DiscordAPIError) {
-				throw new app.Error({
-					message: "Discord API error",
-					args: {
-						message: sendTarget,
-						channelId: channelObject.id,
-						channelName: channelObject.name,
-						guildId: channelObject.guild.id,
-						guildName: channelObject.guild.name
-					}
-				});
-			}
-			else {
-				throw new app.Error({
-					message: "Unknown error",
-					args: {
-						message: sendTarget,
-						channelId: channelObject.id,
-						channelName: channelObject.name,
-						guildId: channelObject.guild.id,
-						guildName: channelObject.guild.name
-					},
-					cause: e
-				});
-			}
-		}
-	}
-	
-	async handleCommand (command, args, channel, user, options = {}) {
-		const execution = await app.Command.checkAndRun(command, args, channel, user, {
-			platform: {
-				id: 0,
-				name: "discord"
-			},
-			...options
-		});
+        try {
+            await channelObject.send(sendTarget);
+        } catch (e) {
+            if (e instanceof DiscordAPIError) {
+                throw new app.Error({
+                    message: "Discord API error",
+                    args: {
+                        message: sendTarget,
+                        channelId: channelObject.id,
+                        channelName: channelObject.name,
+                        guildId: channelObject.guild.id,
+                        guildName: channelObject.guild.name
+                    }
+                });
+            } else {
+                throw new app.Error({
+                    message: "Unknown error",
+                    args: {
+                        message: sendTarget,
+                        channelId: channelObject.id,
+                        channelName: channelObject.name,
+                        guildId: channelObject.guild.id,
+                        guildName: channelObject.guild.name
+                    },
+                    cause: e
+                });
+            }
+        }
+    }
 
-		if (!execution) {
-			return;
-		}
+    async handleCommand(command, args, channel, user, options = {}) {
+        const execution = await app.Command.checkAndRun(command, args, channel, user, {
+            platform: {
+                id: 0,
+                name: "discord"
+            },
+            ...options
+        });
+    
+        if (!execution) {
+            return;
+        }
+    
+        const { reply, tweetLink, tweet } = execution; // Assuming there's a tweetLink and tweet property in the execution result
+    
+        // Construct the tweet message content with @everyone mention at the start
+        const tweetContent = `@everyone New tweet from ${tweet.username}: ${tweet.text}`;
+    
+        const embeds = execution?.discord?.embeds ?? [];
+        if (embeds.length !== 0) {
+            // Add a button to the embed message with custom emoji URL
+            const embed = embeds[0];
+            const button = new MessageActionRow().addComponents(
+                new MessageButton()
+                    .setCustomId('tweet_button')
+                    .setEmoji('https://cdn.discordapp.com/emojis/1129096257986642032.webp?size=96&quality=lossless')
+                    .setLabel('View Tweet')
+                    .setStyle('PRIMARY')
+            );
+    
+            await this.send(tweetContent, channel, { embeds: [embed], components: [button] });
+        } else {
+            await this.send(tweetContent, channel);
+        }
+    }
+    parseMessage(messageObject) {
+        const content = messageObject.content.replace(/<(https?:\/\/.+?)>/g, "$1");
+        const args = content.split(" ");
 
-		const { reply } = execution;
-		const embeds = execution?.discord?.embeds ?? [];
-		if (!reply && embeds.length === 0) {
-			return;
-		}
+        return {
+            message: messageObject.cleanContent.replace(/\s+/g, " "),
+            channel: messageObject.channel.id,
+            channelType: messageObject.channel.type,
+            author: messageObject.author,
+            guild: messageObject?.channel?.guild ?? null,
+            privateMessage: (messageObject.channel.type === ChannelType.DM),
+            commandArgs: args
+        };
+    }
 
-		if (embeds.length !== 0) {
-			await this.send(null, channel, { embeds });
-		}
-		else {
-			await this.send(reply, channel);
-		}
-	}
+    restart() {
+        if (this.client) {
+            for (const [event, listener] of Object.entries(this.client._events)) {
+                this.client.off(event, listener);
+            }
 
-	parseMessage (messageObject) {
-		const content = messageObject.content.replace(/<(https?:\/\/.+?)>/g, "$1");
-		const args = content.split(" ");
+            this.client.destroy();
+        }
 
-		return {
-			message: messageObject.cleanContent.replace(/\s+/g, " "),
-			channel: messageObject.channel.id,
-			channelType: messageObject.channel.type,
-			author: messageObject.author,
-			guild: messageObject?.channel?.guild ?? null,
-			privateMessage: (messageObject.channel.type === ChannelType.DM),
-			commandArgs: args
-		};
-	}
-
-	restart () {
-		if (this.client) {
-			for (const [event, listener] of Object.entries(this.client._events)) {
-				this.client.off(event, listener);
-			}
-
-			this.client.destroy();
-		}
-
-		this.#init();
-	}
+        this.#init();
+    }
 };
--- core/singletons/sentinel/tweet-processor.js
+++ core/singletons/sentinel/tweet-processor.js
@@ -11,58 +11,6 @@ const parseTweet = async (tweet) => {
 	tweetData.text = text;
 	tweetData.createdAt = createdAt;
 
-	if (tweetObject.retweeted_status) {
-		const { screen_name } = tweetObject.entities.user_mentions[0];
-		const retweetData = tweetObject.retweeted_status;
-		const { extended_entities: extendedEntities } = retweetData;
-		if (extendedEntities) {
-			const { media } = extendedEntities;
-			if (media) {
-				const mediaData = media.map((mediaItem) => {
-					const { type, media_url_https: url } = mediaItem;
-					return {
-						type,
-						url
-					};
-				});
-
-				tweetData.media = mediaData;
-			}
-		}
-
-		tweetData.text = `RT @${screen_name}: ${retweetData.full_text}`;
-		tweetData.type = "retweet";
-
-		return tweetData;
-	}
-	else if (tweetObject.is_quote_status && tweet.quoted_status) {
-		const { quoted_status: quotedStatus } = tweetObject;
-		if (quotedStatus?.extended_entities) {
-			const { extended_entities: extendedEntities } = quotedStatus;
-			if (extendedEntities) {
-				const { media } = extendedEntities;
-				if (media) {
-					const mediaData = media.map((mediaItem) => {
-						const { type, media_url_https: url } = mediaItem;
-						return {
-							type,
-							url
-						};
-					});
-
-					tweetData.media = mediaData;
-				}
-			}
-		}
-
-		const { screen_name } = tweetObject.user;
-
-		tweetData.text = `Quote @${screen_name}: ${text}`;
-		tweetData.type = "quote";
-
-		return tweetData;
-	}
-
 	const { extended_entities: extendedEntities } = tweetObject;
 	if (extendedEntities) {
 		const { media } = extendedEntities;
